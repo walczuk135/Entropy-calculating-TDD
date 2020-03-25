b8b70a413350e21c6a87560854d65721be24e0cd
Paweł <walczuk135@gmail.com>
2020-03-26 00:39:35 +0100

    Refactor test FormatterKeyTest

--- src/test/java/service/FormatterKeyTest.java
+++ src/test/java/service/FormatterKeyTest.java
@@ -16,9 +16,17 @@ class FormatterKeyTest {
 
     @Test
     void shouldGetAllFormatterValues() {
+
         //when
         FormatterKey[] values = FormatterKey.values();
         //then
-        assertThat(values).hasSize(7);
+        assertThat(values).hasSize(7)
+            .contains(FormatterKey.DEFAULT_FORMAT)
+            .contains(FormatterKey.WITHOUT_ALPHABETIC)
+            .contains(FormatterKey.WITHOUT_NUMBER)
+            .contains(FormatterKey.WITHOUT_SPECIAL_CHAR)
+            .contains(FormatterKey.WITHOUT_WHITE_CHAR)
+            .contains(FormatterKey.LOWER_CASE)
+            .contains(FormatterKey.UPPER_CASE);
     }
 }
